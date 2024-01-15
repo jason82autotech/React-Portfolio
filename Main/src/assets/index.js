4c4fa67e5f7ff521ad3b61c47de0a2b71cb177a6
Jason Grant <jason82autotech@gmail.com>
2024-01-15 14:09:20 -0800

    added pics and deleted unnecessary files

--- Main/src/assets/index.js
+++ Main/src/assets/index.js
@@ -1,5 +1,1 @@
-export { default as calculator } from './projects/calculator.jpg';
-export { default as ledWall } from './projects/ledWall.jpg';
-export { default as pastelPuzzels } from './projects/pastelPuzzels.jpg';
-export { default as runBuddy } from './projects/runBuddy.jpg';
-export { default as surfReport } from './projects/surfReport.jpg';
+export { default as zion } from './cover/zion.jpg';
